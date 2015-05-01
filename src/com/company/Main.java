cee132ac937882f8fe4268f6ece38812da46614a
ravid_cohen <ravidcohn@gmail.com>
2015-05-01 14:56:29 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("lior wolf is a busster");
         //ertretwetrwet
         //dd
         //dfdf
