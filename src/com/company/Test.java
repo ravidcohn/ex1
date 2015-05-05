639472cf9760d574290acfb68240d6469e793afd
ravid_cohen <ravidcohn@gmail.com>
2015-05-05 15:35:33 +0300

    fucking shit

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -14,25 +14,25 @@ public class Test {
         long time = System.currentTimeMillis();
         double Perplexity = 0;
         double N;
-        int n_gram = 5;
+        int n_gram;
         String method;
         String[] data;
-        HashMap<String,ArrayList<Integer>>[] TN_Table = new HashMap[n_gram];
-        HashMap<String,Integer>[] nGramTable = new HashMap[n_gram];
-        ArrayList<String> corpus = Parse.readCorpus(args[0]);
         data = readData(args[1]);
         method = data[0];
+        n_gram = Integer.parseInt(data[1]);
+        HashMap<String,ArrayList<Double>>[] Table = new HashMap[n_gram];
+        ArrayList<String> corpus = Parse.readCorpus(args[0]);
         N = readNumberOfWords(corpus);
         if (method=="wb"){
-            nGramTable = readNGT(args[1]);
-        }else{
-            TN_Table = readTNT(args[1]);
+            Table = readWB(args[1]);
+        }else if(method=="LS"){
+            Table = readLS(args[1]);
         }
         for (String line: corpus){
             if (method=="wb") {
-                Perplexity += evalLS(line, nGramTable);
-            }else {
-                Perplexity += evalWB(line, TN_Table);
+                Perplexity += evalLS(line, Table);
+            }else if(method=="LS"){
+                Perplexity += evalWB(line, Table);
             }
         }
 
@@ -53,36 +53,40 @@ public class Test {
         return N;
     }
 
-    private static double evalLS(String line, HashMap<String,Integer>[] nGramTable){
+    private static double evalLS(String line, HashMap<String,ArrayList<Double>>[] table){
         //TODO write the function.
         return 0;
     }
 
-    private static double evalWB(String line, HashMap<String,ArrayList<Integer>>[] TN_Table){
+    private static double evalWB(String line, HashMap<String,ArrayList<Double>>[] table){
         //TODO write the function.
         return 0;
     }
 /*
     Read T-N gram table.
  */
-    private static HashMap<String,ArrayList<Integer>>[] readTNT(String path) {
+    private static HashMap<String,ArrayList<Double>>[] readLS(String path) {
         //TODO write the function.
         return null;
     }
 /*
     read n gram table.
  */
-    private static HashMap<String,Integer>[] readNGT(String path) {
+    private static HashMap<String,ArrayList<Double>>[] readWB(String path) {
         BufferedReader br = null;
         String sCurrentLine;
         String line;
+        String[] tokens;
+        int n_gram = 0;
+        String s;
         try {
             br = new BufferedReader(new FileReader(path));
             while ((sCurrentLine = br.readLine()) != null) {
+                line = br.readLine();
+
+                tokens = line.split(" ");
 
             }
-            line = br.readLine();
-            data = line.split(" ");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
