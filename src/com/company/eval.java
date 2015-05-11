8d3ff1933cf05dee4dbf78ef33b2c3ce0105ad72
pinhas <pinhasn@gmail.com>
2015-05-11 19:04:59 +0300

    fuck it 5.0

--- src/com/company/eval.java
+++ src/com/company/eval.java
@@ -4,4 +4,17 @@ package com.company;
  * Created by pinhas on 11/05/2015.
  */
 public class eval {
+
+    public static void main(String[] args) {
+        if(args.length != 4){
+            System.out.println("The program can only run in the following way:");
+            System.out.println("eval -i <input file> -m <model file>");
+        }
+        else{
+            String inPath = args[1];
+            String modelPath = args[3];
+            double Perplexity = 0;
+
+        }
+    }
 }
