99e9ee304c5b8d5159294b3ee01a782a9d506e10
ravid_cohen <ravidcohn@gmail.com>
2015-05-12 02:44:47 +0300

    Merge branch 'master' of https://github.com/ravidcohn/ex1
    Conflicts:
    	src/com/company/test2.java

--- src/com/company/test2.java
+++ src/com/company/test2.java
@@ -13,7 +13,7 @@ public class test2 {
 
     public static void main(String[] args){
 //        ArrayList<Point2D.Double[]>allVals = new ArrayList<>();
-        String method = "wb";//wb\ls.
+        String method = "ls";//wb\ls.
         if (method.equals("ls")) {
             testLS();
         }else if (method.equals("wb")){
@@ -101,26 +101,25 @@ public class test2 {
         for (int i = 2; i < 5; i++) {
             //          Point2D.Double[] vals = lmbdaTestForLidstone("src/en_text.corp","src/en.test",i);
             //   Point2D.Double[] vals = lmbdaTestForLidstone("src/trainLs","src/testLs",i);
-            Point2D.Double[] vals = lmbdaTestForLidstone("src/ca_text.corp", "src/ca.test", i);
-            //            Point2D.Double[] vals = lmbdaTestForLidstone("src/es_text.corp","src/es.test",i);
+            //Point2D.Double[] vals = lmbdaTestForLidstone("src/ca_text.corp", "src/ca.test", i);
+                        Point2D.Double[] vals = lmbdaTestForLidstone("src/es_text.corp","src/es.test",i);
             //          allVals.add(vals);
             System.out.println("n-gram: " + i);
-            GraphPanel.plot(vals);
+    //        GraphPanel.plot(vals);
             for (int j = 0; j < vals.length; j++) {
-                System.out.println("lmbda: " + vals[j].getX() + "   ,Perplexity: " + vals[j].getY());
+            //    System.out.println("lmbda: " + vals[j].getX() + "   ,Perplexity: " + vals[j].getY());
+                System.out.println(vals[j].getY());
             }
         }
     }
 
     public static Point2D.Double[] lmbdaTestForLidstone(String lm_inPath,String test_inPath,int n_gram){
 
-        double start = 0;
-        double end = 1;
-        double jump = 0.1;
-        int size =(int)((end - start)/jump) + 1;
+        double start = 0.000000000001;
+        double end = 0.00001;
+        double jump = 0.000001;
+        int size = 101;
         Point2D.Double[] lmbdaXY = new Point2D.Double[size];
-
-
 
         ArrayList<String> corpus = Parse.readCorpus(lm_inPath);
         String out = "src/lidstonLmbdaTest_";
@@ -133,7 +132,10 @@ public class test2 {
         ArrayList<String> test_corpus = Parse.readCorpus(test_inPath);
 
         double N = eval.readNumberOfWords(test_corpus, n_gram);
-        double[] lmbda = new double[]{0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
+        double[] lmbda = new double[size];
+        for (int i=0;i<=100;i++){
+            lmbda[i] = 0.01*i;
+        }
         for (int count =0; count < lmbda.length; count++) {
             Table = new HashMap[n_gram];
             double Perplexity = 0;
@@ -156,9 +158,9 @@ public class test2 {
         for (int i = 2; i < 5; i++) {
             System.out.println("n-gram: " + i);
 //           best_lambda[i-1]  = lmbdaTestForWB("src/en_text.corp","src/en.test",i);
-                best_lambda[i-1]  = lmbdaTestForWB("src/trainLs","src/testLs",i);
+//                best_lambda[i-1]  = lmbdaTestForWB("src/trainLs","src/testLs",i);
 //            best_lambda[i-1] = lmbdaTestForWB("src/ca_text.corp", "src/ca.test", i);
-//             best_lambda[i-1]  = lmbdaTestForWB("src/es_text.corp","src/es.test",i);
+             best_lambda[i-1]  = lmbdaTestForWB("src/es_text.corp","src/es.test",i);
 
         }
     }
