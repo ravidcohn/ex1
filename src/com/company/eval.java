24242a7cdb61150b955aec35c4678057e5fadc22
pinhas <pinhasn@gmail.com>
2015-05-11 17:52:24 +0300

    fuck it 5.0

---
+++ src/com/company/eval.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by pinhas on 11/05/2015.
+ */
+public class eval {
+}
---
+++ src/com/company/lm.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by pinhas on 11/05/2015.
+ */
+public class lm {
+    public static void main(String [] args){
+        for (int i = 0; i < args.length; i+=2) {
+            if (args[i].equals("-n")){
+
+            }else if(args[i].equals("-i")){
+
+            }
+            else if(args[i].equals("-o")){
+
+            }
+        }
+    }
+}
